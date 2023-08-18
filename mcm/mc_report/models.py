30d6668c57d3b01b4e8c69261f96c8f81c16b679
Yakken-S <yagi.sinfo@gmail.com>
2023-08-18 14:05:40 +0900

    modify models

--- mcm/mc_report/models.py
+++ mcm/mc_report/models.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth import get_user_model 
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
@@ -18,9 +18,16 @@ class L1_Name(models.Model):
 class Issue_History(models.Model):
     l1_name = models.ForeignKey('L1_Name', on_delete=models.CASCADE, related_name="l1_name_issue")
     alarm_num = models.ForeignKey('Alarm', on_delete=models.CASCADE, related_name="alarm_nums_issue")
-    issue_type = models.TextField()
+    issue_types = (
+        ('01','機械不具合'),
+        ('02','電気不具合'),
+        ('03','システム不具合'),
+        ('04','加工精度不良'),
+    )
+    issue_type = models.CharField(max_length=20, choices=issue_types, blank=True)
     issue_title = models.TextField()
     issue_detail = models.TextField()
+    issue_treatment = models.TextField()
     issue_freeinput = models.TextField()
     issue_datetime = models.DateTimeField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -31,10 +38,9 @@ class Issue_History(models.Model):
     file_report  = models.FileField()
     file_image1  = models.FileField()
     file_image2  = models.FileField()
-        
+    
     def __str__(self):
         return f"{self.l1_name} - {self.alarm_num} - {self.issue_title}"
-
 
 class Alarm(models.Model):
     alarm_num = models.CharField(max_length=10)
