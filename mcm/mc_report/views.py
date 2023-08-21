3196169e55a08caa128b2c90c0df738d915df273
Yakken-S <yagi.sinfo@gmail.com>
2023-08-21 16:39:32 +0900

    Modified....

--- mcm/mc_report/views.py
+++ mcm/mc_report/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import L1_Name, Issue_History
 
-# Create your views here.
+# ここでビューを作成します。
 def l1_list(request):
     l1names = L1_Name.objects.all()
-    return render(request, 'mc_report/l1_list.html', {'l1names':l1names})
+    return render(request, 'mc_report/l1_list.html', {'l1names': l1names})
 
 def issue_list(request, pk):
-    issues = get_object_or_404(Issue_History, pk=l1_name)
-    return render(request, 'mc_report/issue_history.html', {'issues': issues})
+    l1_name = get_object_or_404(L1_Name, pk=pk)
+    issues = Issue_History.objects.filter(l1_name=l1_name)
+    return render(request, 'mc_report/issue_list.html', {'issues': issues})
