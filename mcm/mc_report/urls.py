2dd69d853cf6e5bfda4415778e37f10874283eb5
Yakken-S <yagi.sinfo@gmail.com>
2023-08-23 17:21:36 +0900

    Modified....

--- mcm/mc_report/urls.py
+++ mcm/mc_report/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.l1_list, name='l1_list'),
     path('l1name/<int:pk>/', views.issue_list, name='issue_list'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
--- mcm/mc_report/views.py
+++ mcm/mc_report/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import L1_Name, Issue_History
+from .forms import PostForm
 
-# ここでビューを作成します。
 def l1_list(request):
     l1names = L1_Name.objects.all()
     return render(request, 'mc_report/l1_list.html', {'l1names': l1names})
@@ -10,3 +10,4 @@ def issue_list(request, pk):
     l1_name = get_object_or_404(L1_Name, pk=pk)
     issues = Issue_History.objects.filter(l1_name=l1_name)
     return render(request, 'mc_report/issue_list.html', {'issues': issues})
+
