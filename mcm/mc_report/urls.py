f361c12b952f609c5d1fac172c19cb7cfea0e4ed
Yakken-S <yagi.sinfo@gmail.com>
2023-08-29 18:56:21 +0900

    .

--- mcm/mc_report/urls.py
+++ mcm/mc_report/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.l1_list, name='l1_list'),
     path('l1name/<int:pk>/', views.issue_list, name='issue_list'),
-    path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('create/<int:pk>/',views.IssueCreateView.as_view(),name='create'), 
+    # path('post/new/', views.post_new, name='post_new'),
 ]
--- mcm/mc_report/views.py
+++ mcm/mc_report/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import L1_Name, Issue_History
-from .forms import PostForm
+from django.views.generic import (ListView,DetailView,CreateView,DeleteView,UpdateView)
 
 def l1_list(request):
     l1names = L1_Name.objects.all()
@@ -11,3 +11,13 @@ def issue_list(request, pk):
     issues = Issue_History.objects.filter(l1_name=l1_name)
     return render(request, 'mc_report/issue_list.html', {'issues': issues})
 
+# def post_new(request):
+#     form = PostForm()
+#     return render(request, 'mc_report/post_edit.html', {'form': form})
+
+
+class IssueCreateView(CreateView):
+    creates = Issue_History.objects.all()
+    #更新後のリダイレクト先
+    def get_success_url(self):
+        return reverse('mc_report:issues', 'Issue_form.html',kwargs={'pk': self.object.pk})
