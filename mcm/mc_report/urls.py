25e9c002d5e5d7441a2c7739873a8ddfcf178834
Yakken-S <yagi.sinfo@gmail.com>
2023-09-01 13:10:32 +0900

    Modified html url

--- mcm/mc_report/urls.py
+++ mcm/mc_report/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.l1_list, name='l1_list'),
     path('<int:pk>/', views.issue_list, name='issue_list'),
-    path('', views.post_issue, name='post_issue'),
+    path('post', views.post_issue, name='post_issue'),
 ]
